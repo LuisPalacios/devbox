a3a0483c65130895cc9d284d11b5070b09f1fee4
Luis Palacios <luis.palacios.derqui@gmail.com>
2021-06-18 11:26:01 +0200

    Repito

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -309,23 +309,28 @@ function servicioJupyterLab {
 }
 
 function instalarKite {
-sudo -u "${CONF_usuario}" -i bash _ "${CONF_usuario}" <<'EOF_KITE'
-    echo "Instalo Kite como usuario '${1}'"
+    echo "Instalo Kite Engine"
+
+sudo -u "${CONF_usuario}" -i bash  _ "${CONF_usuario}" <<'EOF_KITE'
+
+    echo "Instalo Kite Engine como usuario '${1}'"
     cd /home/${1}
     
     loginctl enable-linger luis
-
+    
     wget -q https://linux.kite.com/dls/linux/current -O kite-installer.sh
+    
     chmod a+x ./kite-installer.sh
     bash ./kite-installer.sh --download > /dev/null
     bash ./kite-installer.sh --install > /dev/null
     rm -f ./kite-installer.sh
-
+    
     systemctl daemon-reload
     systemctl --user enable /home/${1}/.config/systemd/user/kite-autostart.service
     systemctl --user enable /home/${1}/.config/systemd/user/kite-updater.timer
-
+    
 EOF_KITE
+
 }
 
 #
